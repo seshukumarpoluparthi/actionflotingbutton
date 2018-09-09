69e3e102669e0bdf5c10190ee024d4be074e8fdd
yogeshpatel4me <yogeshpatel@yogeshs-macbook-air-2.local>
2018-09-09 20:35:58 +0530

    Initial Commit

---
+++ actionflotingbutton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  actionflotingbutton
+//
+//  Created by Yogesh Patel on 09/09/18.
+//  Copyright © 2018 Yogesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ actionflotingbuttonTests/actionflotingbuttonTests.swift
@@ -0,0 +1,36 @@
+//
+//  actionflotingbuttonTests.swift
+//  actionflotingbuttonTests
+//
+//  Created by Yogesh Patel on 09/09/18.
+//  Copyright © 2018 Yogesh Patel. All rights reserved.
+//
+
+import XCTest
+@testable import actionflotingbutton
+
+class actionflotingbuttonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
