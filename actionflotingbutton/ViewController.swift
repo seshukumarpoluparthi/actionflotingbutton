db4e4f9824cffb6009b1eefb8eef5af747aa9185
apple <appleapple@apples-macbook-air.local>
2018-09-12 17:03:39 +0530

    Floating Button

--- actionflotingbutton/ViewController.swift
+++ actionflotingbutton/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var actionButton : ActionButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupButtons()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupButtons(){
+        let google = ActionButtonItem(title: "Google", image: #imageLiteral(resourceName: "a1"))
+        google.action = { item in self.view.backgroundColor = UIColor.red }
+        
+        google.action = { item in
+            
+            
+            let stbd = UIStoryboard(name: "Main", bundle: nil)
+            let vc = stbd.instantiateViewController(withIdentifier: "TestViewController") as! TestViewController
+            self.present(vc, animated: true, completion: nil)
+            
+            
+        }
+        
+        
+        let twitter = ActionButtonItem(title: "Twitter", image: #imageLiteral(resourceName: "a2"))
+        twitter.action = { item in self.view.backgroundColor = UIColor.blue }
+        let facebook = ActionButtonItem(title: "Facebook", image: #imageLiteral(resourceName: "a3"))
+        let linkedin = ActionButtonItem(title: "Linkedin", image: #imageLiteral(resourceName: "a4"))
+        actionButton = ActionButton(attachedToView: self.view, items: [google, twitter, facebook, linkedin])
+        actionButton.setTitle("+", forState: UIControlState())
+        actionButton.backgroundColor = UIColor(red: 238.0/255.0, green: 130.0/255.0, blue: 130.0/255.0, alpha: 1)
+        actionButton.action = { button in button.toggleMenu()}
     }
-
-
 }
 
